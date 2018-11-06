06bfec238526cf1c0f029d1d112af51d00b257ab
Brian4280 <36721294+Brian4280@users.noreply.github.com>
2018-11-05 18:06:12 -0800

    MT1 Submit

---
+++ TIVAC/Midterm1/Project/Debug/main.d
@@ -0,0 +1,49 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/stdarg.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/stdbool.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/stdint.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/stdlib.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/linkage.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/stdio.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/stdarg.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/tm4c123gh6pm.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/hw_i2c.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/hw_memmap.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/hw_types.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/hw_gpio.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/i2c.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/sysctl.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/gpio.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/pin_map.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/uart.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/utils/uartstdio.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/stdarg.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/interrupt.h
+main.obj: ../Adafruit_TSL2591.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/timer.h
+
+../main.c: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/stdarg.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/stdbool.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/stdint.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/stdlib.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/linkage.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/stdio.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/stdarg.h: 
+C:/ti/tivaware_c_series_2_1_4_178/inc/tm4c123gh6pm.h: 
+C:/ti/tivaware_c_series_2_1_4_178/inc/hw_i2c.h: 
+C:/ti/tivaware_c_series_2_1_4_178/inc/hw_memmap.h: 
+C:/ti/tivaware_c_series_2_1_4_178/inc/hw_types.h: 
+C:/ti/tivaware_c_series_2_1_4_178/inc/hw_gpio.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/i2c.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/sysctl.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/gpio.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/pin_map.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/uart.h: 
+C:/ti/tivaware_c_series_2_1_4_178/utils/uartstdio.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/stdarg.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/interrupt.h: 
+../Adafruit_TSL2591.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/timer.h: 
---
+++ TIVAC/Midterm1/Project/Debug/utils/subdir_rules.mk
@@ -0,0 +1,15 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Each subdirectory must supply rules for building sources it contributes
+utils/uartstdio.obj: C:/ti/tivaware_c_series_2_1_4_178/utils/uartstdio.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM Compiler'
+	"C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="C:/Brian/Fall 2018/CPE403/Workspace/Midterm" --include_path="C:/ti/tivaware_c_series_2_1_4_178" --include_path="C:/ti/tivaware_c_series_2_1_4_178/utils" --include_path="C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include" --define=ccs="ccs" --define=PART_TM4C123GH6PM -g --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="utils/uartstdio.d" --obj_directory="utils" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
